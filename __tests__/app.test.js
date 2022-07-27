270381c8702233260eb6bba8edad8db3b1d19bef
Julie Nisbet <jmnisbet@gmail.com>
2022-07-27 11:15:16 -0700

    adding tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /dogs should return a list of dogs', async () => {
+    const resp = await request(app).get('/dogs');
+    expect(resp.body.length).toEqual(4);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+  it('GET /dogs/:id should return the dog detail', async () => {
+    const resp = await request(app).get('/dogs/1');
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'Benny',
+      age: 7,
+    });
   });
   afterAll(() => {
     pool.end();
